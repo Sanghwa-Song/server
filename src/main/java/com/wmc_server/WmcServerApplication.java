6ddc487b687db318f87a69add2f9b78af9795c7b
hyun-seung <78414370+hyun-seung@users.noreply.github.com>
2022-04-05 10:24:38 +0900

    main

---
+++ src/main/java/com/wmc_server/WmcServerApplication.java
@@ -0,0 +1,13 @@
+package com.wmc_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WmcServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WmcServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wmc_server/WmcServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wmc_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WmcServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
